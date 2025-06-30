8ed3c1950fc56369dc08ba72c06d7a78d2570652
Dinuki85 <prarthanadinuki2@gmail.com>
2025-06-30 16:24:36 +0530

    Added more files

---
+++ app/pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <section className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-4xl font-outfit">Welcome to Barco Logistics</h1>
+        <p className="mt-4 text-lg italic font-ovo">
+          Delivering excellence worldwide.
+        </p>
+      </section>
+    </div>
+  );
+}
