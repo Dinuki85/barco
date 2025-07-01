baf029b6f453758bfa4803ff2541e92d2e1622af
Dinuki85 <prarthanadinuki2@gmail.com>
2025-07-01 16:55:37 +0530

    Feat:add contact page with start content

---
+++ app/pages/contact/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <main  className='flex flex-col items-center justify-center min-h-screen bg-gray-100 '>
+            <h1 className='mb-4 text-3xl font-bold'>Contact Us</h1>
+            <p className='max-w-xl text-lg text-center'>If you have any questions, feel free to reach out to us!</p> 
+        </main>
+      
+    </div>
+  )
+}
+
+export default page
