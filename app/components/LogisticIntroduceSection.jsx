c4fb7a61926601df495e405f0336a97e74ab07fe
Dinuki85 <prarthanadinuki2@gmail.com>
2025-07-03 22:32:10 +0530

    Update:Updated the code with linking button to the services

--- app/components/LogisticIntroduceSection.jsx
+++ app/components/LogisticIntroduceSection.jsx
@@ -3,10 +3,11 @@ import { Grandstander } from 'next/font/google';
 import { useEffect, useState } from 'react'
 import { CgAdd } from "react-icons/cg";
 import Counter from './Counter';
+import Link from 'next/link';
 
 const LogisticIntroduceSection = () => {
-    
-    
+
+
     return (
         <section className='relative grid w-full min-h-screen grid-cols-2 justfy-center item-s-center bg:light-blue-100 md:items-center sm:items-center'>
             <div className='absolute  grid-cols-2 place-items-start w-full h-full inset-1 bg:light-blue-500 sm:w-full sm:h-full md:w-full md:h-full *:'>
@@ -32,29 +33,33 @@ const LogisticIntroduceSection = () => {
                     capabilities to handle it all.
                 </p>
                 <ul className='grid grid-cols-3 gap-4 px-3 py-3 font-serif text-lg list-none sm:text-base md:text-lg text-mono'>
-                     <li className='flex px-2 py-1 flex-center'>  <CgAdd />    Warehousing and Distribution</li>
-                     <li  className='flex px-2 py-1 flex-center'>  <CgAdd /> Freight Forwarding</li>
-                   <li  className='flex px-2 py-1 flex-center'> <CgAdd /> Supply Chain Management</li>
-                      <li  className='flex px-2 py-1 flex-center'> <CgAdd /> E commerce Logistics</li>
-                     <li  className='flex px-2 py-1 flex-center'> <CgAdd/>  Last-Mile Delivery</li>
-                   <li className='flex px-2 py-1 flex-center'>  <CgAdd />    Warehousing and Distribution</li>
-                     <li  className='flex px-2 py-1 flex-center'>  <CgAdd /> Freight Forwarding</li>
-                   <li  className='flex px-2 py-1 flex-center'> <CgAdd /> Supply Chain Management</li>
-                      <li  className='flex px-2 py-1 flex-center'> <CgAdd /> E commerce Logistics</li>
-                     <li  className='flex px-2 py-1 flex-center'> <CgAdd/>  Last-Mile Delivery</li>
+                    <li className='flex px-2 py-1 flex-center'>  <CgAdd />    Warehousing and Distribution</li>
+                    <li className='flex px-2 py-1 flex-center'>  <CgAdd /> Freight Forwarding</li>
+                    <li className='flex px-2 py-1 flex-center'> <CgAdd /> Supply Chain Management</li>
+                    <li className='flex px-2 py-1 flex-center'> <CgAdd /> E commerce Logistics</li>
+                    <li className='flex px-2 py-1 flex-center'> <CgAdd />  Last-Mile Delivery</li>
+                    <li className='flex px-2 py-1 flex-center'>  <CgAdd />    Warehousing and Distribution</li>
+                    <li className='flex px-2 py-1 flex-center'>  <CgAdd /> Freight Forwarding</li>
+                    <li className='flex px-2 py-1 flex-center'> <CgAdd /> Supply Chain Management</li>
+                    <li className='flex px-2 py-1 flex-center'> <CgAdd /> E commerce Logistics</li>
+                    <li className='flex px-2 py-1 flex-center'> <CgAdd />  Last-Mile Delivery</li>
                 </ul>
 
-               <button className='block px-4 py-2 mx-auto text-center text-white bg-blue-500 rounded-md shadow-md hover:bg-blue-600 focus:outline-double focus:ring-2 focus:ring-blue-500 focus:ring-opacity-75'>Explore Our services</button>
+                <button className='block px-4 py-2 mx-auto text-center text-white bg-blue-500 rounded-md shadow-md hover:bg-blue-600 focus:outline-double focus:ring-2 focus:ring-blue-500 focus:ring-opacity-75'>
+                    <Link href="/services">
+                        Explore Our services
+                    </Link>
+                </button>
 
 
             </div>
-            
+
             {/*<div className="flex w-full px-10 py-10 gid-rows-2 h -screen jstify-center i2tems-center inset- flex-center bg-light-blue-100 sm:w-full bottom-9">
         <button className='border rounborder-spacing-1 border-darkBlue '><Counter targetNumber={1000} /></button>  
         </div>
             */}
         </section>
-        
+
 
     )
 }
