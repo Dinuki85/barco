3635af68981bd954db65e9d3a5ada963dcd4d37b
Dinuki85 <prarthanadinuki2@gmail.com>
2025-07-19 21:05:45 +0530

    Feat:Added counter function

--- app/components/LogisticIntroduceSection.jsx
+++ app/components/LogisticIntroduceSection.jsx
@@ -54,6 +54,24 @@ const h2LetterVariants = {
     visible: { opacity: 1, y: 0 }
 };
 
+function useCounter(targetNumber, duration = 2000) {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        let start = 0;
+        const end = parseInt(targetNumber);
+        if (start === end) return;
+        let incrementTime = Math.max(10, Math.floor(duration / end));
+        let timer = setInterval(() => {
+            start += 1;
+            setCount(start);
+            if (start === end) clearInterval(timer);
+        }, incrementTime);
+        return () => clearInterval(timer);
+    }, [targetNumber, duration]);
+
+    return count;
+}
 const LogisticIntroduceSection = () => {
 
 
