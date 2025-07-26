62e766313e80dbdbc7d0ac2820664e52f1b9fd10
Dinuki85 <prarthanadinuki2@gmail.com>
2025-07-26 23:02:32 +0530

    Fix:Remove the dupliacte header from the contact us page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 export default function ContactPage() {
   return (
     <>
-      <Header />
+     
       <main className="min-h-screen px-4 pt-24 bg-gray-50">
         
         <ContactSection />
